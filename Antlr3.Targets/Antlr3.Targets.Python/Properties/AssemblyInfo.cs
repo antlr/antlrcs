9e073ff8a1a1462ccafe19b4ed8b2e80cd502555
sharwell <sharwell@pixelminegames.com>
2009-11-02 11:48:06 -0800

    C# Port: Updated assembly revision numbers to 3.2.0.6288 for today's build.
    [git-p4: depot-paths = "//depot/code/antlrcs/main/": change = 6292]

--- Antlr3.Targets/Antlr3.Targets.Python/Properties/AssemblyInfo.cs
+++ Antlr3.Targets/Antlr3.Targets.Python/Properties/AssemblyInfo.cs
@@ -66,5 +66,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "3.1.3.6002" )]
-[assembly: AssemblyFileVersion( "3.1.0.0" )]
+[assembly: AssemblyVersion("3.2.0.6288")]
+[assembly: AssemblyFileVersion("3.2.0.6288")]
