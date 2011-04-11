d3f20e4058be5c2c8d3559d6e098bd954d96770a
sharwell <sharwell@pixelminegames.com>
2011-04-10 18:00:14 -0800

    (C# 3) Forgot to add file
    [git-p4: depot-paths = "//depot/code/antlrcs/main/": change = 8156]

---
+++ Antlr4.StringTemplate/Misc/AttributeNotFoundException.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * [The "BSD license"]
+ *  Copyright (c) 2011 Terence Parr
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *  3. The name of the author may not be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ *  IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ *  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ *  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ *  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace Antlr4.StringTemplate.Misc
+{
+    using Antlr4.StringTemplate.Compiler;
+
+    ////** <name> where name is not found up the dynamic scoping chain. */
+    public class AttributeNotFoundException : TemplateException
+    {
+        private readonly TemplateFrame _frame;
+        private readonly string _attributeName;
+
+        public AttributeNotFoundException(TemplateFrame frame, string attributeName)
+        {
+            _frame = frame;
+            _attributeName = attributeName;
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "from template " + _frame.Template.Name + " no attribute " + _attributeName + " is visible";
+            }
+        }
+    }
+}
