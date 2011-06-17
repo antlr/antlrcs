0a723aa03fbc82eb8949f3016a998959f09f98d2
sharwell <sharwell@pixelminegames.com>
2011-06-17 07:08:29 -0800

    (C# 3) Move most inline grammar actions to the helper file
    [git-p4: depot-paths = "//depot/code/antlrcs/main/": change = 8654]

--- Antlr3/Grammars/DefineGrammarItemsWalkerHelper.cs
+++ Antlr3/Grammars/DefineGrammarItemsWalkerHelper.cs
@@ -33,6 +33,7 @@
 namespace Antlr3.Grammars
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using Antlr.Runtime.Tree;
     using Antlr3.Tool;
@@ -98,6 +99,232 @@ namespace Antlr3.Grammars
             if ( r != null )
             {
                 r.TrackInlineAction( actionAST );
+            }
+        }
+
+        private void HandleAttrScope(GrammarAST name, GrammarAST attrs, IDictionary<GrammarAST, GrammarAST> actions)
+        {
+            AttributeScope scope = grammar.DefineGlobalScope(name.Text, attrs.Token);
+            scope.IsDynamicGlobalScope = true;
+            scope.AddAttributes(attrs.Text, ';');
+            foreach (var action in actions)
+                scope.DefineNamedAction(action.Key, action.Value);
+        }
+
+        private void HandleAttrScopeAction(GrammarAST id, GrammarAST action, IDictionary<GrammarAST, GrammarAST> actions)
+        {
+            actions.Add(id, action);
+        }
+
+        private void HandleAction(string scope, GrammarAST amp, GrammarAST name, GrammarAST action)
+        {
+            grammar.DefineNamedAction(amp, scope, name, action);
+        }
+
+        private void HandleRuleAfterOptionsSpec(ref string name, ref Rule r, IDictionary<string, object> opts, GrammarAST start, GrammarAST id, string modifier, GrammarAST args, GrammarAST ret, HashSet<string> exceptions)
+        {
+            name = id.Text;
+            currentRuleName = name;
+            if (Rule.GetRuleType(name) == RuleType.Lexer && grammar.type == GrammarType.Combined)
+            {
+                // a merged grammar spec, track lexer rules and send to another grammar
+                grammar.DefineLexerRuleFoundInParser(id.Token, start);
+            }
+            else
+            {
+                int numAlts = CountAltsForRule(start);
+                grammar.DefineRule(id.Token, modifier, opts, start, args, numAlts);
+                r = grammar.GetRule(name);
+                if (args != null)
+                {
+                    r.ParameterScope = grammar.CreateParameterScope(name, args.Token);
+                    r.ParameterScope.AddAttributes(args.Text, ',');
+                }
+
+                if (ret != null)
+                {
+                    r.ReturnScope = grammar.CreateReturnScope(name, ret.Token);
+                    r.ReturnScope.AddAttributes(ret.Text, ',');
+                }
+
+                if (exceptions != null)
+                {
+                    foreach (string exception in exceptions)
+                        r.ThrowsSpec.Add(exception);
+                }
+            }
+        }
+
+        private void HandleRuleAfterRuleActions()
+        {
+            blockLevel = 0;
+        }
+
+        private void HandleRuleEnd(GrammarAST blockStart, IDictionary<string, object> opts)
+        {
+            // copy rule options into the block AST, which is where
+            // the analysis will look for k option etc...
+            blockStart.BlockOptions = opts;
+        }
+
+        private void HandleRuleAction(Rule r, GrammarAST amp, GrammarAST id, GrammarAST action)
+        {
+            if (r != null)
+                r.DefineNamedAction(amp, id, action);
+        }
+
+        private void HandleRuleScopeSpecAction(Rule r, GrammarAST attrs, IDictionary<GrammarAST, GrammarAST> actions)
+        {
+            r.RuleScope = grammar.CreateRuleScope(r.Name, attrs.Token);
+            r.RuleScope.IsDynamicRuleScope = true;
+            r.RuleScope.AddAttributes(attrs.Text, ';');
+            foreach (var action in actions)
+                r.RuleScope.DefineNamedAction(action.Key, action.Value);
+        }
+
+        private void HandleRuleScopeSpecUses(Rule r, GrammarAST uses)
+        {
+            if (grammar.GetGlobalScope(uses.Text) == null)
+            {
+                ErrorManager.GrammarError(ErrorManager.MSG_UNKNOWN_DYNAMIC_SCOPE, grammar, uses.Token, uses.Text);
+            }
+            else
+            {
+                if (r.UseScopes == null)
+                    r.UseScopes = new List<string>();
+
+                r.UseScopes.Add(uses.Text);
+            }
+        }
+
+        private void HandleElementAssign(GrammarAST id, GrammarAST elementStart)
+        {
+            if (elementStart.Type == ANTLRParser.ROOT || elementStart.Type == ANTLRParser.BANG)
+            {
+                elementStart = (GrammarAST)elementStart.GetChild(0);
+            }
+
+            if (elementStart.Type == RULE_REF)
+            {
+                grammar.DefineRuleRefLabel(currentRuleName, id.Token, elementStart);
+            }
+            else if (elementStart.Type == WILDCARD && grammar.type == GrammarType.TreeParser)
+            {
+                grammar.DefineWildcardTreeLabel(currentRuleName, id.Token, elementStart);
+            }
+            else
+            {
+                grammar.DefineTokenRefLabel(currentRuleName, id.Token, elementStart);
+            }
+        }
+
+        private void HandleElementPlusAssign(GrammarAST id, GrammarAST elementStart)
+        {
+            if (elementStart.Type == ANTLRParser.ROOT || elementStart.Type == ANTLRParser.BANG)
+            {
+                elementStart = (GrammarAST)elementStart.GetChild(0);
+            }
+
+            if (elementStart.Type == RULE_REF)
+            {
+                grammar.DefineRuleListLabel(currentRuleName, id.Token, elementStart);
+            }
+            else if (elementStart.Type == WILDCARD && grammar.type == GrammarType.TreeParser)
+            {
+                grammar.DefineWildcardTreeListLabel(currentRuleName, id.Token, elementStart);
+            }
+            else
+            {
+                grammar.DefineTokenListLabel(currentRuleName, id.Token, elementStart);
+            }
+        }
+
+        private void HandleInlineAction(GrammarAST action)
+        {
+            action.outerAltNum = outerAltNum;
+            TrackInlineAction(action);
+        }
+
+        private void HandleDotLoop(GrammarAST start)
+        {
+            GrammarAST block = (GrammarAST)start.GetChild(0);
+            IDictionary<string, object> opts = new Dictionary<string, object>();
+            opts["greedy"] = "false";
+            if (grammar.type != GrammarType.Lexer)
+            {
+                // parser grammars assume k=1 for .* loops otherwise they (analysis?) look til EOF!
+                opts["k"] = 1;
+            }
+
+            block.SetOptions(grammar, opts);
+        }
+
+        private void HandleRuleReferenceAtom(GrammarAST scope, GrammarAST ruleReference, GrammarAST action)
+        {
+            grammar.AltReferencesRule(currentRuleName, scope, ruleReference, this.outerAltNum);
+            if (action != null)
+            {
+                action.outerAltNum = this.outerAltNum;
+                TrackInlineAction(action);
+            }
+        }
+
+        private void HandleTokenReferenceAtom(GrammarAST scope, GrammarAST tokenReference, GrammarAST action)
+        {
+            if (action != null)
+            {
+                action.outerAltNum = this.outerAltNum;
+                TrackInlineAction(action);
+            }
+
+            if (grammar.type == GrammarType.Lexer)
+            {
+                grammar.AltReferencesRule(currentRuleName, scope, tokenReference, this.outerAltNum);
+            }
+            else
+            {
+                grammar.AltReferencesTokenID(currentRuleName, tokenReference, this.outerAltNum);
+            }
+        }
+
+        private void HandleStringLiteralAtom(GrammarAST literal)
+        {
+            if (grammar.type != GrammarType.Lexer)
+            {
+                Rule rule = grammar.GetRule(currentRuleName);
+                if (rule != null)
+                    rule.TrackTokenReferenceInAlt(literal, outerAltNum);
+            }
+        }
+
+        private void HandleRewriteAtomStart(GrammarAST start)
+        {
+            Rule r = grammar.GetRule(currentRuleName);
+            var tokenRefsInAlt = r.GetTokenRefsInAlt(outerAltNum);
+            bool imaginary = start.Type == TOKEN_REF && !tokenRefsInAlt.Contains(start.Text);
+            if (!imaginary && grammar.BuildAST)
+            {
+                switch (start.Type)
+                {
+                case RULE_REF:
+                case LABEL:
+                case TOKEN_REF:
+                case CHAR_LITERAL:
+                case STRING_LITERAL:
+                    // track per block and for entire rewrite rule
+                    if (currentRewriteBlock != null)
+                    {
+                        currentRewriteBlock.rewriteRefsShallow.Add(start);
+                        currentRewriteBlock.rewriteRefsDeep.Add(start);
+                    }
+
+                    //System.out.println("adding "+$start.Text+" to "+currentRewriteRule.Text);
+                    currentRewriteRule.rewriteRefsDeep.Add(start);
+                    break;
+
+                default:
+                    break;
+                }
             }
         }
     }
--- Antlr3/Grammars/TreeToNFAConverterHelper.cs
+++ Antlr3/Grammars/TreeToNFAConverterHelper.cs
@@ -39,6 +39,7 @@ namespace Antlr3.Grammars
     using ArgumentNullException = System.ArgumentNullException;
     using CommonTreeNodeStream = Antlr.Runtime.Tree.CommonTreeNodeStream;
     using IIntSet = Antlr3.Misc.IIntSet;
+    using IntervalSet = Antlr3.Misc.IntervalSet;
     using ITreeNodeStream = Antlr.Runtime.Tree.ITreeNodeStream;
 
     partial class TreeToNFAConverter
@@ -175,6 +176,417 @@ namespace Antlr3.Grammars
                 throw new ArgumentNullException("node");
 
             return node.terminalOptions != null && node.terminalOptions.Count > 0;
+        }
+
+        private void HandleRuleId(GrammarAST id)
+        {
+            currentRuleName = id.Text;
+            factory.CurrentRule = grammar.GetLocallyDefinedRule(currentRuleName);
+        }
+
+        private void HandleRule(GrammarAST start, StateCluster g, GrammarAST blockStart, GrammarAST id)
+        {
+            if (blockStart.SetValue != null)
+            {
+                // if block comes back as a set not BLOCK, make it
+                // a single ALT block
+                g = factory.BuildAlternativeBlockFromSet(g);
+            }
+
+            if (Rule.GetRuleType(currentRuleName) == RuleType.Parser || grammar.type == GrammarType.Lexer)
+            {
+                // attach start node to block for this rule
+                Rule thisR = grammar.GetLocallyDefinedRule(currentRuleName);
+                NFAState start2 = thisR.StartState;
+                start2.associatedASTNode = id;
+                start2.AddTransition(new Transition(Label.EPSILON, g.Left));
+
+                // track decision if > 1 alts
+                if (grammar.GetNumberOfAltsForDecisionNFA(g.Left) > 1)
+                {
+                    g.Left.Description = grammar.GrammarTreeToString(start, false);
+                    g.Left.SetDecisionASTNode(blockStart);
+                    int d = grammar.AssignDecisionNumber(g.Left);
+                    grammar.SetDecisionNFA(d, g.Left);
+                    grammar.SetDecisionBlockAST(d, blockStart);
+                }
+
+                // hook to end of rule node
+                NFAState end = thisR.StopState;
+                g.Right.AddTransition(new Transition(Label.EPSILON, end));
+            }
+        }
+
+        private StateCluster HandleAlternativeElement(StateCluster g, StateCluster element)
+        {
+            return factory.BuildAB(g, element);
+        }
+
+        private StateCluster HandleAlternativeEnd(StateCluster g)
+        {
+            if (g == null)
+            {
+                // if alt was a list of actions or whatever
+                g = factory.BuildEpsilon();
+            }
+            else
+            {
+                factory.OptimizeAlternative(g);
+            }
+
+            return g;
+        }
+
+        private StateCluster HandleEbnfSet(StateCluster g)
+        {
+            return g;
+        }
+
+        private StateCluster HandleEbnfBlock(GrammarAST blk, StateCluster g)
+        {
+            // track decision if > 1 alts
+            if (grammar.GetNumberOfAltsForDecisionNFA(g.Left) > 1)
+            {
+                g.Left.Description = grammar.GrammarTreeToString(blk, false);
+                g.Left.SetDecisionASTNode(blk);
+                int d = grammar.AssignDecisionNumber(g.Left);
+                grammar.SetDecisionNFA(d, g.Left);
+                grammar.SetDecisionBlockAST(d, blk);
+            }
+
+            return g;
+        }
+
+        private StateCluster HandleEbnfOptionalBlock(GrammarAST start, GrammarAST blk, StateCluster bg)
+        {
+            if (blk.SetValue != null)
+            {
+                // if block comes back SET not BLOCK, make it
+                // a single ALT block
+                bg = factory.BuildAlternativeBlockFromSet(bg);
+            }
+
+            StateCluster g = factory.BuildAoptional(bg);
+            g.Left.Description = grammar.GrammarTreeToString(start, false);
+            // there is always at least one alt even if block has just 1 alt
+            int d = grammar.AssignDecisionNumber(g.Left);
+            grammar.SetDecisionNFA(d, g.Left);
+            grammar.SetDecisionBlockAST(d, blk);
+            g.Left.SetDecisionASTNode(start);
+            return g;
+        }
+
+        private StateCluster HandleEbnfClosureBlock(GrammarAST start, GrammarAST blk, StateCluster bg)
+        {
+            GrammarAST eob = blk.LastChild;
+            if (blk.SetValue != null)
+            {
+                bg = factory.BuildAlternativeBlockFromSet(bg);
+            }
+
+            StateCluster g = factory.BuildAstar(bg);
+            // track the loop back / exit decision point
+            bg.Right.Description = "()* loopback of " + grammar.GrammarTreeToString(start, false);
+            int d = grammar.AssignDecisionNumber(bg.Right);
+            grammar.SetDecisionNFA(d, bg.Right);
+            grammar.SetDecisionBlockAST(d, blk);
+            bg.Right.SetDecisionASTNode(eob);
+            // make block entry state also have same decision for interpreting grammar
+            NFAState altBlockState = (NFAState)g.Left.GetTransition(0).Target;
+            altBlockState.SetDecisionASTNode(start);
+            altBlockState.DecisionNumber = d;
+            g.Left.DecisionNumber = d; // this is the bypass decision (2 alts)
+            g.Left.SetDecisionASTNode(start);
+            return g;
+        }
+
+        private StateCluster HandleEbnfPositiveClosureBlock(GrammarAST start, GrammarAST blk, StateCluster bg)
+        {
+            GrammarAST eob = blk.LastChild;
+            if (blk.SetValue != null)
+            {
+                bg = factory.BuildAlternativeBlockFromSet(bg);
+            }
+
+            StateCluster g = factory.BuildAplus(bg);
+            // don't make a decision on left edge, can reuse loop end decision
+            // track the loop back / exit decision point
+            bg.Right.Description = "()+ loopback of " + grammar.GrammarTreeToString(start, false);
+            int d = grammar.AssignDecisionNumber(bg.Right);
+            grammar.SetDecisionNFA(d, bg.Right);
+            grammar.SetDecisionBlockAST(d, blk);
+            bg.Right.SetDecisionASTNode(eob);
+            // make block entry state also have same decision for interpreting grammar
+            NFAState altBlockState = (NFAState)g.Left.GetTransition(0).Target;
+            altBlockState.SetDecisionASTNode(start);
+            altBlockState.DecisionNumber = d;
+            return g;
+        }
+
+        private StateCluster HandleTreeFirstElement(GrammarAST firstElementStart, StateCluster element, out StateCluster down)
+        {
+            down = factory.BuildAtom(Label.DOWN, firstElementStart);
+            // TODO set following states for imaginary nodes?
+            //el.followingNFAState = down.Right;
+            return factory.BuildAB(element, down);
+        }
+
+        private StateCluster HandleTreeElement(StateCluster g, StateCluster element)
+        {
+            return factory.BuildAB(g, element);
+        }
+
+        private StateCluster HandleTreeAfterLastElement(GrammarAST start, StateCluster g, GrammarAST lastElementStart, StateCluster down)
+        {
+            StateCluster up = factory.BuildAtom(Label.UP, lastElementStart);
+            //el.followingNFAState = up.Right;
+            g = factory.BuildAB(g, up);
+            // tree roots point at right edge of DOWN for LOOK computation later
+            start.NFATreeDownState = down.Left;
+
+            return g;
+        }
+
+        private StateCluster HandleNotAtomCharLiteral(GrammarAST notNode, GrammarAST charLiteral)
+        {
+            int ttype = 0;
+            if (grammar.type == GrammarType.Lexer)
+            {
+                ttype = Grammar.GetCharValueFromGrammarCharLiteral(charLiteral.Text);
+            }
+            else
+            {
+                ttype = grammar.GetTokenType(charLiteral.Text);
+            }
+
+            IIntSet notAtom = grammar.Complement(ttype);
+            if (notAtom.IsNil)
+            {
+                ErrorManager.GrammarError(
+                    ErrorManager.MSG_EMPTY_COMPLEMENT,
+                    grammar,
+                    charLiteral.Token,
+                    charLiteral.Text);
+            }
+
+            return factory.BuildSet(notAtom, notNode);
+        }
+
+        private StateCluster HandleNotAtomTokenReference(GrammarAST notNode, GrammarAST tokenReference)
+        {
+            int ttype = 0;
+            IIntSet notAtom = null;
+            if (grammar.type == GrammarType.Lexer)
+            {
+                notAtom = grammar.GetSetFromRule(this, tokenReference.Text);
+                if (notAtom == null)
+                {
+                    ErrorManager.GrammarError(
+                        ErrorManager.MSG_RULE_INVALID_SET,
+                        grammar,
+                        tokenReference.Token,
+                        tokenReference.Text);
+                }
+                else
+                {
+                    notAtom = grammar.Complement(notAtom);
+                }
+            }
+            else
+            {
+                ttype = grammar.GetTokenType(tokenReference.Text);
+                notAtom = grammar.Complement(ttype);
+            }
+
+            if (notAtom == null || notAtom.IsNil)
+            {
+                ErrorManager.GrammarError(
+                    ErrorManager.MSG_EMPTY_COMPLEMENT,
+                    grammar,
+                    tokenReference.Token,
+                    tokenReference.Text);
+            }
+
+            return factory.BuildSet(notAtom, notNode);
+        }
+
+        private StateCluster HandleNotAtomSet(GrammarAST notNode, GrammarAST setNode)
+        {
+            //IIntSet notSet = grammar.Complement(stNode.SetValue);
+            // let code generator complement the sets
+            IIntSet s = setNode.SetValue;
+            setNode.SetValue = s;
+            // let code gen do the complement again; here we compute
+            // for NFA construction
+            s = grammar.Complement(s);
+            if (s.IsNil)
+            {
+                ErrorManager.GrammarError(
+                    ErrorManager.MSG_EMPTY_COMPLEMENT,
+                    grammar,
+                    notNode.Token);
+            }
+
+            return factory.BuildSet(s, notNode);
+        }
+
+        private void HandleNotAtomEnd(GrammarAST notNode, StateCluster g)
+        {
+            notNode.followingNFAState = g.Right;
+        }
+
+        private StateCluster HandleAtomRuleReference(string scopeName, GrammarAST ruleReference)
+        {
+            NFAState start = grammar.GetRuleStartState(scopeName, ruleReference.Text);
+            if (start != null)
+            {
+                Rule rr = grammar.GetRule(scopeName, ruleReference.Text);
+                StateCluster g = factory.BuildRuleRef(rr, start);
+                ruleReference.followingNFAState = g.Right;
+                ruleReference._nfaStartState = g.Left;
+                if (g.Left.GetTransition(0) is RuleClosureTransition && grammar.type != GrammarType.Lexer)
+                {
+                    AddFollowTransition(ruleReference.Text, g.Right);
+                }
+                // else rule ref got inlined to a set
+
+                return g;
+            }
+
+            return null;
+        }
+
+        private StateCluster HandleAtomTokenReference(string scopeName, GrammarAST tokenReference)
+        {
+            if (grammar.type == GrammarType.Lexer)
+            {
+                NFAState start = grammar.GetRuleStartState(scopeName, tokenReference.Text);
+                if (start != null)
+                {
+                    Rule rr = grammar.GetRule(scopeName, tokenReference.Text);
+                    StateCluster g = factory.BuildRuleRef(rr, start);
+                    tokenReference._nfaStartState = g.Left;
+                    // don't add FOLLOW transitions in the lexer;
+                    // only exact context should be used.
+                    return g;
+                }
+
+                return null;
+            }
+            else
+            {
+                StateCluster g = factory.BuildAtom(tokenReference);
+                tokenReference.followingNFAState = g.Right;
+                return g;
+            }
+        }
+
+        private StateCluster HandleAtomCharLiteral(GrammarAST charLiteral)
+        {
+            if (grammar.type == GrammarType.Lexer)
+            {
+                return factory.BuildCharLiteralAtom(charLiteral);
+            }
+            else
+            {
+                StateCluster g = factory.BuildAtom(charLiteral);
+                charLiteral.followingNFAState = g.Right;
+                return g;
+            }
+        }
+
+        private StateCluster HandleAtomStringLiteral(GrammarAST stringLiteral)
+        {
+            if (grammar.type == GrammarType.Lexer)
+            {
+                return factory.BuildStringLiteralAtom(stringLiteral);
+            }
+            else
+            {
+                StateCluster g = factory.BuildAtom(stringLiteral);
+                stringLiteral.followingNFAState = g.Right;
+                return g;
+            }
+        }
+
+        private StateCluster HandleAtomWildcard(GrammarAST wildcard)
+        {
+            if (nfa.Grammar.type == GrammarType.TreeParser
+                && (wildcard.ChildIndex > 0 || wildcard.Parent.GetChild(1).Type == EOA))
+            {
+                return factory.BuildWildcardTree(wildcard);
+            }
+            else
+            {
+                return factory.BuildWildcard(wildcard);
+            }
+        }
+
+        private void HandleSetElementCharLiteral(IIntSet elements, GrammarAST c)
+        {
+            int ttype;
+            if (grammar.type == GrammarType.Lexer)
+                ttype = Grammar.GetCharValueFromGrammarCharLiteral(c.Text);
+            else
+                ttype = grammar.GetTokenType(c.Text);
+
+            if (elements.Contains(ttype))
+                ErrorManager.GrammarError(ErrorManager.MSG_DUPLICATE_SET_ENTRY, grammar, c.Token, c.Text);
+
+            elements.Add(ttype);
+        }
+
+        private void HandleSetElementTokenReference(IIntSet elements, GrammarAST t)
+        {
+            int ttype;
+            if (grammar.type == GrammarType.Lexer)
+            {
+                // recursively will invoke this rule to match elements in target rule ref
+                IIntSet ruleSet = grammar.GetSetFromRule(this, t.Text);
+                if (ruleSet == null)
+                    ErrorManager.GrammarError(ErrorManager.MSG_RULE_INVALID_SET, grammar, t.Token, t.Text);
+                else
+                    elements.AddAll(ruleSet);
+            }
+            else
+            {
+                ttype = grammar.GetTokenType(t.Text);
+                if (elements.Contains(ttype))
+                    ErrorManager.GrammarError(ErrorManager.MSG_DUPLICATE_SET_ENTRY, grammar, t.Token, t.Text);
+
+                elements.Add(ttype);
+            }
+        }
+
+        private void HandleSetElementStringLiteral(IIntSet elements, GrammarAST s)
+        {
+            int ttype = grammar.GetTokenType(s.Text);
+            if (elements.Contains(ttype))
+                ErrorManager.GrammarError(ErrorManager.MSG_DUPLICATE_SET_ENTRY, grammar, s.Token, s.Text);
+
+            elements.Add(ttype);
+        }
+
+        private void HandleSetElementCharRange(IIntSet elements, GrammarAST c1, GrammarAST c2)
+        {
+            if (grammar.type == GrammarType.Lexer)
+            {
+                int a = Grammar.GetCharValueFromGrammarCharLiteral(c1.Text);
+                int b = Grammar.GetCharValueFromGrammarCharLiteral(c2.Text);
+                elements.AddAll(IntervalSet.Of(a, b));
+            }
+        }
+
+        private void HandleSetElementSet(IIntSet elements, StateCluster g)
+        {
+            Transition setTrans = g.Left.GetTransition(0);
+            elements.AddAll(setTrans.Label.Set);
+        }
+
+        private void HandleSetElementNotSetElement(IIntSet elements, IIntSet ns)
+        {
+            IIntSet not = grammar.Complement(ns);
+            elements.AddAll(not);
         }
     }
 }
