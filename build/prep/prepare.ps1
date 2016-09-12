ddec68653a67e7bda21979e3b3c5e7f8ab81f911
Sam Harwell <sam@tunnelvisionlabs.com>
2016-09-12 18:12:50 -0500

    Updated assembly versions for ST4 to 4.0.8

--- build/prep/prepare.ps1
+++ build/prep/prepare.ps1
@@ -205,7 +205,7 @@ copy "LICENSE.txt" ".\ST4"
 
 # compress the distributable packages
 $AntlrVersion = "3.5.0.3-dev"
-$STVersion = "4.0.7.2-dev"
+$STVersion = "4.0.8.0-dev"
 
 $ArchivePath = ".\dist\antlr-dotnet-csharpbootstrap-" + $AntlrVersion + ".7z"
 .\7z.exe a -r -mx9 $ArchivePath ".\Bootstrap\*"
