f74532f821b727f23b622ab64043c1bb8c2a36a9
Sam Harwell <sam@tunnelvisionlabs.com>
2017-07-30 00:00:38 -0500

    Convert Antlr4.StringTemplate.Visualizer to the new project system

--- Antlr4.StringTemplate.Visualizer/Properties/AssemblyInfo.cs
+++ Antlr4.StringTemplate.Visualizer/Properties/AssemblyInfo.cs
@@ -31,21 +31,9 @@
  */
 
 using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Antlr4.StringTemplate.Visualizer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Tunnel Vision Laboratories, LLC")]
-[assembly: AssemblyProduct("Antlr4.StringTemplate.Visualizer")]
-[assembly: AssemblyCopyright("Copyright © Sam Harwell 2013")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(false)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -71,18 +59,3 @@ using System.Windows;
     //(used if a resource is not found in the page, 
     // app, or any theme specific resource dictionaries)
 )]
-
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.7.0")]
-[assembly: AssemblyFileVersion("4.0.9.0")]
-[assembly: AssemblyInformationalVersion("4.0.9-dev")]
