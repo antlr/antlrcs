9fc8e926b99ef6bf7384b095a114708087184732
Sam Harwell <sam@tunnelvisionlabs.com>
2017-07-31 23:49:24 -0500

    Remove the use of AppDomain in the build tasks

--- AntlrBuildTask/AntlrClassGenerationTask.cs
+++ AntlrBuildTask/AntlrClassGenerationTask.cs
@@ -33,8 +33,6 @@ namespace Antlr3.Build.Tasks
     using System.Diagnostics;
     using System.Linq;
     using System.Reflection;
-    using System.Security;
-    using System.Security.Policy;
     using System.Threading;
     using Microsoft.Build.Framework;
     using Microsoft.Build.Utilities;
@@ -46,7 +44,7 @@ namespace Antlr3.Build.Tasks
     public class AntlrClassGenerationTask
         : Task
     {
-        private static AppDomain _sharedAppDomain;
+        private static readonly SemaphoreSlim _lock = new SemaphoreSlim(1);
 
         private const string DefaultGeneratedSourceExtension = "g";
         private List<ITaskItem> _generatedCodeFiles = new List<ITaskItem>();
@@ -143,26 +141,8 @@ namespace Antlr3.Build.Tasks
             }
         }
 
-        public AppDomain GetAntlrTaskAppDomain()
-        {
-            if (_sharedAppDomain != null)
-                return _sharedAppDomain;
-
-            AppDomainSetup info = new AppDomainSetup
-            {
-                ApplicationBase = BuildTaskPath,
-                LoaderOptimization = LoaderOptimization.MultiDomainHost,
-                ShadowCopyFiles = "true"
-            };
-
-            string friendlyName = "AntlrClassGenerationDomain_" + Guid.NewGuid();
-            _sharedAppDomain = AppDomain.CreateDomain(friendlyName, AppDomain.CurrentDomain.Evidence, info, new NamedPermissionSet("FullTrust"), new StrongName[0]);
-            return _sharedAppDomain;
-        }
-
         public override bool Execute()
         {
-            AppDomain domain = null;
             bool success;
 
             if (!Path.IsPathRooted(AntlrToolPath))
@@ -173,9 +153,17 @@ namespace Antlr3.Build.Tasks
 
             try
             {
-                domain = GetAntlrTaskAppDomain();
-                AntlrClassGenerationTaskInternal wrapper = CreateBuildTaskWrapper(domain);
-                success = wrapper.Execute();
+                AntlrClassGenerationTaskInternal wrapper = CreateBuildTaskWrapper();
+
+                _lock.Wait();
+                try
+                {
+                    success = wrapper.Execute();
+                }
+                finally
+                {
+                    _lock.Release();
+                }
 
                 if (success)
                 {
@@ -194,11 +182,6 @@ namespace Antlr3.Build.Tasks
 
                 ProcessExceptionAsBuildMessage(exception);
                 success = false;
-            }
-            finally
-            {
-                if (domain != null && domain != _sharedAppDomain)
-                    AppDomain.Unload(domain);
             }
 
             return success;
@@ -240,9 +223,9 @@ namespace Antlr3.Build.Tasks
             }
         }
 
-        private AntlrClassGenerationTaskInternal CreateBuildTaskWrapper(AppDomain domain)
+        private AntlrClassGenerationTaskInternal CreateBuildTaskWrapper()
         {
-            AntlrClassGenerationTaskInternal wrapper = (AntlrClassGenerationTaskInternal)domain.CreateInstanceAndUnwrap(Assembly.GetExecutingAssembly().FullName, typeof(AntlrClassGenerationTaskInternal).FullName);
+            var wrapper = new AntlrClassGenerationTaskInternal();
 
             IList<string> sourceCodeFiles = null;
             if (this.SourceCodeFiles != null)
